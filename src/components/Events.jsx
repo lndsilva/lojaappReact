e5155698537a591ca5dfc2b77fcda1ff675de6d2
lndsilva <lndsilva@hotmail.com>
2023-09-25 16:59:15 -0300

    Evento de clique dentro de uma função

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,11 +1,19 @@
 const Events = () => {
+  const HandleClick = (e) => {
+    console.log(e);
+    console.log("Executou");
+  };
+
   return (
     <div>
-        <button onClick={
-            () => console.log("Testando um evento de clique")
-        }>Clique aqui</button>
+      <button onClick={() => console.log("Testando um evento de clique")}>
+        Clique aqui
+      </button>
+      <div>
+        <button onClick={HandleClick}>Clique aqui - com função</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
